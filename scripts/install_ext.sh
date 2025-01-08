6757cb91c10d37cd265fdb74948ac746431916bc
PorcupineBee <shanubiswas091199@gmail.com>
2025-01-08 16:00:25 +0530

    Update install_ext.sh

--- scripts/install_ext.sh
+++ scripts/install_ext.sh
@@ -1,4 +1,4 @@
 pip install ./raymarching
-pip install ./shencoder
-pip install ./freqencoder
-pip install ./gridencoder
+TORCH_CUDA_ARCH_LIST="5.0 6.0 7.0 7.5 8.0 8.6 9.0+PTX" pip install ./shencoder
+TORCH_CUDA_ARCH_LIST="5.0 6.0 7.0 7.5 8.0 8.6 9.0+PTX" pip install ./freqencoder
+TORCH_CUDA_ARCH_LIST="5.0 6.0 7.0 7.5 8.0 8.6 9.0+PTX" pip install ./gridencoder
